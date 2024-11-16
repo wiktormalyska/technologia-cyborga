67baedb87d2dd6670bb05e97d8fb2bf06ee19890
Wiktor Małyska <wiktormalyska03@gmail.com>
2024-11-16 23:59:41 +0100

    Added run configs

--- backend/src/main/java/ovh/wiktormalyska/backend/BackendApplication.java
+++ backend/src/main/java/ovh/wiktormalyska/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.kamw.backend;
+package ovh.wiktormalyska.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/ovh/wiktormalyska/backend/BackendApplicationTests.java
+++ backend/src/test/java/ovh/wiktormalyska/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kamw.backend;
+package ovh.wiktormalyska.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
