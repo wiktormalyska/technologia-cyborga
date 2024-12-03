f18407b93c54987ab1d60e5ea1d4b74f8ed5ae8c
Wiktor Małyska <wiktormalyska03@gmail.com>
2024-12-03 13:27:17 +0100

    Added URL for DB Fix

--- backend/src/main/java/ovh/wiktormalyska/backend/BackendApplication.java
+++ backend/src/main/java/ovh/wiktormalyska/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package ovh.wiktormalyska.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Set system properties for Spring
+		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+		System.setProperty("spring.datasource.driver-class-name", dotenv.get("DATABASE_DRIVER_CLASS"));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
