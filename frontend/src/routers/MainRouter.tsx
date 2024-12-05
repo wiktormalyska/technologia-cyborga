a30880cc7a9fc7f95356c0097094cd113666a3c3
Wiktor Małyska <wiktormalyska03@gmail.com>
2024-12-05 10:31:35 +0100

    fix .idea

--- frontend/src/routers/MainRouter.tsx
+++ frontend/src/routers/MainRouter.tsx
@@ -1,18 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
-import MainLayout from '../components/MainLayout'; // Importuj layout
+import MainLayout from '../components/MainLayout';
 
 const MainRouter = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout />,
         children: [
-            { index: true, element: <h1>Home</h1> }, // Strona główna
+            { index: true, element: <h1>Home</h1> },
             { path: 'friends', element: <h1>Friends</h1>},
-            {path: 'games', element: <h1>Games</h1>},
+            { path: 'games', element: <h1>Games</h1>},
             { path: 'lootboxes', element: <h1>Lootboxes</h1>},
-            {path: 'rankings', element: <h1>Rankings</h1>},
+            { path: 'rankings', element: <h1>Rankings</h1>},
             { path: 'account', element: <h1>Account</h1>},
-            {path: 'settings', element: <h1>Settings</h1>},
+            { path: 'settings', element: <h1>Settings</h1>},
         ],
     },
 ]);
