950c6685ecabde4a83b85e1aa3a4eec05871e49b
Wiktor Małyska <wiktormalyska03@gmail.com>
2024-12-05 12:36:21 +0100

    add datasources and login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,12 +1,29 @@
 import {BasePage} from "../components/BasePage";
 import styled from "styled-components";
+import {useAuth} from "../auth/AuthContext";
+import {useState} from "react";
 
 export const LoginPage = () => {
+    const { login } = useAuth()
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const handleLogin = () => {
+        if (username === "admin" && password === "admin") {
+            login()
+        } else {
+            setError("Invalid username or password.")
+        }
+    }
+
     return (
         <LoginComponent>
             <Text>Login</Text>
-            <Input></Input>
-            <Input></Input>
+            <Input placeholder="Login" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
+            <Input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
+            <Input type="button" value="Login" onClick={handleLogin}/>
+            {error && <ErrorText>{error}</ErrorText>}
         </LoginComponent>
     )
 }
@@ -16,12 +33,26 @@ const LoginComponent = styled(BasePage)`
     display: flex;
     flex-direction: column;
     align-content: center;
-    align-items: center
+    align-items: center;
+    background-color: #1d1b20;
+    width: auto;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    color: white;
 `
 
 const Text = styled.span`
-    
+    padding-bottom: 5rem;
+    font-weight: 700;
+    font-size: 1.5rem;
 `
 const Input = styled.input`
     width: 200px;
+    padding: 5px;
 `
+
+const ErrorText = styled.span`
+    color: red;
+    font-size: 0.9rem;
+    margin-top: 10px;
+`;
