62cdbccdaac40818dd1c69d6884e71b5ed865d51
MikolajMys <mikolaj.my@gmail.com>
2025-01-05 19:29:24 +0100

    Add Friend entity, service, and controller, connect with User entity

--- backend/src/main/java/ovh/wiktormalyska/backend/controller/FriendController.java
+++ backend/src/main/java/ovh/wiktormalyska/backend/controller/FriendController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//:)
 @RestController
 @RequestMapping("/api/friends")
 public class FriendController {
--- backend/src/main/java/ovh/wiktormalyska/backend/repository/FriendRepository.java
+++ backend/src/main/java/ovh/wiktormalyska/backend/repository/FriendRepository.java
@@ -3,7 +3,7 @@ package ovh.wiktormalyska.backend.repository;
 import ovh.wiktormalyska.backend.model.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-
+//:)
 public interface FriendRepository extends JpaRepository<Friend, Long> {
     List<Friend> findByUserId(Long userId);
 }
