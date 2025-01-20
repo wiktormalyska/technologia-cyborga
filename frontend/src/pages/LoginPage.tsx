0a9433a4ab4821316dba9d0fae9fefc10c044a16
JPomorski <kuba.pomor@gmail.com>
2025-01-20 23:35:45 +0100

    TC-83: Login page redesign

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,6 +2,9 @@ import {BasePage} from "../components/BasePage";
 import styled from "styled-components";
 import {useAuth} from "../auth/AuthContext";
 import {useState} from "react";
+// @ts-ignore
+import robotIcon from '../assets/icons/robot.svg'
+import colorPalette from "../values/colorPalette";
 
 export const LoginPage = () => {
     const { login } = useAuth()
@@ -18,15 +21,39 @@ export const LoginPage = () => {
     }
 
     return (
-        <LoginComponent>
-            <Text>Login</Text>
-            <Input placeholder="Login" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
-            <Input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
-            <Input type="button" value="Login" onClick={handleLogin}/>
-            {error && <ErrorText>{error}</ErrorText>}
-        </LoginComponent>
+        <LoginContainer>
+            <LoginComponent>
+                <LoginHeader>
+                    <Text>Login</Text>
+                </LoginHeader>
+                <LoginSection>
+                    <Input placeholder="Login" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
+                    <Input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
+                    <LoginButton type="button" value="Login" onClick={handleLogin}/>
+                </LoginSection>
+                <ErrorSection>
+                    {error && <ErrorText>{error}</ErrorText>}
+                </ErrorSection>
+            </LoginComponent>
+            <LogoSection>
+                <img src={robotIcon} alt="Robot" />
+            </LogoSection>
+        </LoginContainer>
     )
 }
+
+const LoginContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+    padding: 5rem;
+    box-sizing: border-box;
+    align-content: center;
+    align-items: center;
+    overflow: hidden;
+    background-color: ${colorPalette.pageBackground.hex};
+`
 
 const LoginComponent = styled(BasePage)`
     gap: 10px;
@@ -35,24 +62,92 @@ const LoginComponent = styled(BasePage)`
     align-content: center;
     align-items: center;
     background-color: #1d1b20;
-    width: auto;
+    width: 600px;
+    height: 360px;
     padding: 1rem;
     border-radius: 0.5rem;
     color: white;
 `
 
-const Text = styled.span`
-    padding-bottom: 5rem;
-    font-weight: 700;
-    font-size: 1.5rem;
+const LoginHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    margin-bottom: 2rem;
+    width: 100%;
 `
+
+const LoginSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    gap: 1rem;
+`
+
+const ErrorSection = styled(LoginSection)`
+    height: 20px;
+`
+
+const LogoSection = styled(LoginComponent)`
+    width: 420px;
+    height: 420px;
+    background-color: rgba(255,255,255,0);
+
+    img {
+        height: 100%;
+        width: 100%;
+        padding: 12px;
+    }
+`
+
+const Text = styled.span`
+    padding-left: 2rem;
+    text-align: left;
+    font-weight: 700;
+    font-size: 2rem;
+    width: 100%;
+`
+
 const Input = styled.input`
-    width: 200px;
-    padding: 5px;
+    width: 80%;
+    height: 42px;
+    padding: 5px 5px 5px 1rem;
+    border-radius: 0.5rem;
+    border: none;
+    gap: 1rem;
+    font-size: 1rem;
+`
+
+const LoginButton = styled(Input)`
+    background-color: #6200ea;
+    padding: 0;
+    border: none;
+    color: white;
+
+    &:hover {
+        background-color: #5200c6;
+        cursor: pointer;
+    }
 `
 
 const ErrorText = styled.span`
     color: red;
     font-size: 0.9rem;
     margin-top: 10px;
+    
+    animation: slideIn 0.3s ease;
+
+    @keyframes slideIn {
+        from {
+            opacity: 0;
+            transform: translateY(-10px);
+        }
+        
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
 `;
--- frontend/src/values/colorPalette.tsx
+++ frontend/src/values/colorPalette.tsx
@@ -2,12 +2,13 @@ interface Color {
     hex: string;
 }
 
-const colorPalette: { [key in 'primary' | 'secondary' | 'accent' | 'text' | 'secondaryText' | 'inputBackground' | 'buttonBackground' | 'border' | 'highlight']: Color } = {
+const colorPalette: { [key in 'primary' | 'secondary' | 'accent' | 'text' | 'secondaryText' | 'pageBackground' | 'inputBackground' | 'buttonBackground' | 'border' | 'highlight']: Color } = {
     primary: { hex: "#2C2333" }, // Dominujący fioletowy
     secondary: { hex: "#383040" }, // Tło dla elementów
     accent: { hex: "#FFFFFF" }, // Kolor tekstu i ikon
     text: { hex: "#D0D0D0" }, // Jasnoszary tekst
     secondaryText: { hex: "#D0D0D0" },
+    pageBackground: { hex: "#313142" }, // Tło dla strony
     inputBackground: { hex: "#31293A" }, // Pole wyszukiwania
     buttonBackground: { hex: "#453651" }, // Tło przycisków
     border: { hex: "#5C4D6A" }, // Obwódki
