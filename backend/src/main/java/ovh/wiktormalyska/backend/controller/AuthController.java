46163a29f6d15d02014e1209aad418ad66d8f820
JPomorski <kuba.pomor@gmail.com>
2025-02-25 19:35:40 +0100

    TC-79: Add roles to JWT token and put it in cookie

--- backend/src/main/java/ovh/wiktormalyska/backend/controller/AuthController.java
+++ backend/src/main/java/ovh/wiktormalyska/backend/controller/AuthController.java
@@ -1,5 +1,7 @@
 package ovh.wiktormalyska.backend.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ovh.wiktormalyska.backend.dto.AuthResponseDto;
 import ovh.wiktormalyska.backend.dto.LoginDto;
+import ovh.wiktormalyska.backend.security.JwtTokenProvider;
 import ovh.wiktormalyska.backend.service.AuthService;
 
 @AllArgsConstructor
@@ -19,11 +22,19 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
     @PostMapping("/login")
-    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
         String token = authService.login(loginDto);
+
+        Cookie jwtCookie = jwtTokenProvider.createJwtCookie(token);
+        response.addCookie(jwtCookie);
+
         AuthResponseDto authResponseDto = new AuthResponseDto();
         authResponseDto.setAccessToken(token);
+
         System.out.println("User logged in:" + loginDto.getUsername());
         return new ResponseEntity<>(authResponseDto, HttpStatus.OK);
     }
