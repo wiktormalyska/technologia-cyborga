b616cd0c229aa93aaaedb8a0e40963a8d89f47b4
Wiktor Małyska <wiktormalyska03@gmail.com>
2025-02-26 16:14:56 +0100

    move to TailwindCss

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import {Link} from "react-router-dom";
 import * as React from "react";
 // @ts-ignore
@@ -22,156 +21,51 @@ import logoutIcon from '../assets/icons/logout-icon.svg'
 
 const Navbar = () => {
     const menuOptions = [
-        { name: 'All chats', icon: chatsIcon , path: '/'},
-        { name: 'Friends', icon: friendsIcon, path: '/friends'},
-        { name: 'Games', icon: gamesIcon, path: '/games'},
-        { name: 'Lootboxes', icon: lootboxesIcon, path: '/lootboxes'},
-        { name: 'Rankings', icon: rankingsIcon, path: '/rankings'},
-        { name: 'Account', icon: accountIcon, path: '/account'},
-        { name: 'Settings', icon: settingsIcon, path: '/settings'},
+        {name: 'All chats', icon: chatsIcon, path: '/'},
+        {name: 'Friends', icon: friendsIcon, path: '/friends'},
+        {name: 'Games', icon: gamesIcon, path: '/games'},
+        {name: 'Lootboxes', icon: lootboxesIcon, path: '/lootboxes'},
+        {name: 'Rankings', icon: rankingsIcon, path: '/rankings'},
+        {name: 'Account', icon: accountIcon, path: '/account'},
+        {name: 'Settings', icon: settingsIcon, path: '/settings'},
     ];
 
     return (
-        <NavbarContainer>
-            <AppName>
-                <img src={robotIcon} alt="Robot"/>
-                <p>Cyborg App</p>
-            </AppName>
-            <Divider/>
-            <Menu>
+        <div className={"flex flex-col justify-start items-center " +
+            "pt-3 pb-3 pl-4 pr-4 bg-neutral-900 h-full w-[250px]"}>
+            <div className={"flex flex-row items-center text-center gap-2 mb-4 mt-4"}>
+                <img className={"h-[40px] w-[40px] bg-neutral-900"}
+                     src={robotIcon} alt="Robot"/>
+                <p className={"pl-2 text-text text-2xl tracking-wide"}>Cyborg App</p>
+            </div>
+            <div className={"w-full h-[1px] bg-neutral-600 mt-4 mb-4 m-0"}/>
+            <div className={"flex flex-col gap-5"}>
                 {menuOptions.map((option, index) => (
-                    <MenuItem key={index} to={option.path}>
-                        <img src={option.icon} alt={option.name}/>
+                    <Link className={"flex flex-row items-center p-2 gap-4 cursor-pointer " +
+                        "hover:bg-neutral-800 bg-neutral-900 duration-200 " +
+                        "w-full rounded-lg"}
+                          key={index} to={option.path}>
+                        <img className={"h-[40px] w-[40px]" +
+                            "transition-all duration-300"}
+                             src={option.icon} alt={option.name}/>
                         <p>{option.name}</p>
-                    </MenuItem>
+                    </Link>
                 ))}
-            </Menu>
-            <Logout key={"logout"} to={"/logout"}>
-                    <img src={logoutIcon} alt={"logout"}/>
-                    <p>Logout</p>
-            </Logout>
-        </NavbarContainer>
+            </div>
+            <Link className={"flex items-center flex-col content-center " +
+                "justify-end flex-nowrap font-bold mt-auto w-[50%] p-4 " +
+                " bg-neutral-900 hover:bg-neutral-800 duration-200 rounded-lg" +
+                " transition-all"}
+                  key={"logout"} to={"/logout"}>
+                <img
+                    className={"h-[30px] w-[30px] "}
+                    src={logoutIcon}
+                    alt="logout"
+                />
+                <p className={"text-lg text-text"}>Logout</p>
+            </Link>
+        </div>
     );
 };
 
 export default Navbar;
-
-const Logout = styled(Link)`
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    align-content: center;
-    justify-content: flex-end;
-    flex-wrap: nowrap;
-    font-weight: 400;
-    margin-top: auto;
-    width: 50%;
-    padding: 10px;
-
-    &:hover {
-        background-color: #2c2a2f;
-        border-radius: 0.8rem;
-
-        img {
-            background-color: #2c2a2f;
-        }
-        
-    }
-
-    img {
-        height: 30px;
-        width: 30px;
-        background-color: #1d1b20;
-        border-radius: 0.5rem;
-        transition: background-color;
-    }
-
-    p {
-        font-size: 17px;
-        font-family: 'Josefin Sans', sans-serif;
-        color: #ffffff;
-    }
-
-`
-
-const NavbarContainer = styled.div`
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: center;
-      padding: 0.65rem 1rem;
-      background-color: #1d1b20;
-      height: 100%;
-      width: 250px;
-    
-    
-`;
-
-const AppName = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: flex-end;
-    gap: 0.4rem;
-    margin-bottom: 1rem;
-    margin-top: 1rem;
-
-    img {
-        height: 40px;
-        width: 40px;
-        background-color: #1d1b20;
-    }
-
-    p {
-        padding-left: 10px;
-        font-size: 28px;
-        color: white;
-    }
-`;
-
-const Divider = styled.div`
-    width: 100%;
-    height: 1px;
-    background-color: grey;
-    opacity: 1;
-    margin: 1rem 0;
-`;
-
-const Menu = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 1.25rem;
-`;
-
-const MenuItem = styled(Link)`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 0.5rem;
-    gap: 1rem;
-    cursor: pointer;
-
-    &:hover {
-        background-color: #2c2a2f; 
-        border-radius: 0.8rem;
-        width: 100%;
-        
-        img {
-            background-color: #2c2a2f; 
-        }
-    }
-
-    img {
-        height: 40px;
-        width: 40px;
-        background-color: #1d1b20;
-        border-radius: 0.5rem;
-        transition: background-color; 
-        
-    }
-
-    p {
-        font-size: 18px;
-        font-family: 'Josefin Sans', sans-serif;
-        color: #ffffff;
-    }
-`;
