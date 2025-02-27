3ecde617e3ff8d030597c7c23da99156e0ecd7d4
Wiktor Małyska <wikimal200310@gmail.com>
2025-02-28 00:40:59 +0100

    add profileImagePath to User Response

--- backend/src/main/java/ovh/wiktormalyska/backend/service/UserService.java
+++ backend/src/main/java/ovh/wiktormalyska/backend/service/UserService.java
@@ -51,6 +51,7 @@ public class UserService {
                 .username(user.getUsername())
                 .email(user.getEmail())
                 .roles(user.getRoles())
+                .profileImagePath(user.getProfileImagePath())
                 .build();
     }
 
