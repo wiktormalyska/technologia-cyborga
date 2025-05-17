493ca71235fb240c9a02b38af6a43c973a69df57
wiktormalyska <wiktormalyska03@gmail.com>
2025-05-17 22:53:01 +0200

    Remove unused Controller import in LootBoxController

--- backend/src/main/java/ovh/wiktormalyska/backend/controller/LootBoxController.java
+++ backend/src/main/java/ovh/wiktormalyska/backend/controller/LootBoxController.java
@@ -2,7 +2,6 @@ package ovh.wiktormalyska.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
