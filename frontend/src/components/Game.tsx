6ac868a768ce22447b817fe713729a8568afa5fa
Amelka <126866176+amelia-kowalik@users.noreply.github.com>
2025-05-28 16:09:57 +0200

    Add communication between app and game

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,6 +1,13 @@
 import { Unity, useUnityContext } from "react-unity-webgl";
 import {useEffect, useState} from "react";
+import {useAuth} from "../auth/AuthContext";
+import {useCurrentUser} from "../hooks/useAuth";
 
+declare global {
+    interface Window {
+        dispatchReactUnityEvent?: (eventName: string, data: any) => void;
+    }
+}
 function GameComponent() {
     const { unityProvider } = useUnityContext({
         loaderUrl: "/unity/HiLoBuild.loader.js",
@@ -11,16 +18,37 @@ function GameComponent() {
 
     const [score, setScore] = useState(0);
 
+
+    const { user, isAuthenticated } = useCurrentUser();
+
     useEffect(() => {
-        const handleSetScore = (newScore) => {
-            setScore(newScore);
+        window.dispatchReactUnityEvent = function (eventName, data) {
+            if (eventName === "SetScore") {
+                setScore(data);
+            }
         };
 
-        addEventListener("SetScore", handleSetScore);
         return () => {
-            removeEventListener("SetScore", handleSetScore);
+            delete window.dispatchReactUnityEvent;
         };
-    }, [addEventListener, removeEventListener]);
+    }, []);
+
+    useEffect(() => {
+        if (score != null && user?.id != null) {
+            fetch(`http://localhost:8080/api/users/${user.id}/points?points=${score}`, {
+                method: "POST",
+                headers: {
+                    //"Authorization": `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                }
+            })
+                .then(res => {
+                    if (!res.ok) throw new Error("Failed to update user points");
+                    console.log("User points updated successfully");
+                })
+                .catch(console.error);
+        }
+    }, [score, user]);
 
     return (
         <div style={{ width: "100%", height: "750px" }}>
